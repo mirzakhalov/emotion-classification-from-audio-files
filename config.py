ca2a88d0a83931ace01ca748c5f96921ff00e9ab
Jamshidbek Mirzakhalov <mirzakhalov@techs-MBP-3.home>
2021-05-25 23:55:22 -0400

    added steps to format properly

--- config.py
+++ config.py
@@ -24,9 +24,11 @@ if sys.platform.startswith('win32'):
     MODEL_DIR_PATH = str(working_dir_path) + '\\model\\'
     TESS_ORIGINAL_FOLDER_PATH = str(working_dir_path) + '\\TESS_Toronto_emotional_speech_set_data\\'
     EXAMPLES_PATH = str(working_dir_path) + '\\examples\\'
+    DATA_PATH = str(working_dir_path) + "\\data\\"
 else:
     TRAINING_FILES_PATH = str(working_dir_path) + '/features/'
     SAVE_DIR_PATH = str(working_dir_path) + '/joblib_features/'
     MODEL_DIR_PATH = str(working_dir_path) + '/model/'
     TESS_ORIGINAL_FOLDER_PATH = str(working_dir_path) + '/TESS_Toronto_emotional_speech_set_data/'
     EXAMPLES_PATH = str(working_dir_path) + '/examples/'
+    DATA_PATH = str(working_dir_path) + "/data/"
--- live_predictions.py
+++ live_predictions.py
@@ -1,10 +1,11 @@
 """
 This file can be used to try a live prediction. 
 """
-
-import keras
+import tensorflow as tf
+from tensorflow import keras
 import librosa
 import numpy as np
+import os
 
 from config import EXAMPLES_PATH
 from config import MODEL_DIR_PATH
@@ -15,11 +16,11 @@ class LivePredictions:
     Main class of the application.
     """
 
-    def __init__(self, file):
+    def __init__(self, folder):
         """
         Init method is used to initialize the main parameters.
         """
-        self.file = file
+        self.files = os.listdir(folder)
         self.path = MODEL_DIR_PATH + 'Emotion_Voice_Detection_Model.h5'
         self.loaded_model = keras.models.load_model(self.path)
 
@@ -27,12 +28,28 @@ class LivePredictions:
         """
         Method to process the files and create your features.
         """
-        data, sampling_rate = librosa.load(self.file)
-        mfccs = np.mean(librosa.feature.mfcc(y=data, sr=sampling_rate, n_mfcc=40).T, axis=0)
-        x = np.expand_dims(mfccs, axis=2)
-        x = np.expand_dims(x, axis=0)
-        predictions = self.loaded_model.predict_classes(x)
-        print( "Prediction is", " ", self.convert_class_to_emotion(predictions))
+        fields = ['Filename', 'Prediction Class', 'Confidence'] 
+        csvfile = open("output.csv", 'w')
+
+        # creating a csv writer object 
+        csvwriter = csv.writer(csvfile) 
+        csvwriter.writerow(fields) 
+    
+
+
+        for file in self.files:
+            data, sampling_rate = librosa.load(file)
+            mfccs = np.mean(librosa.feature.mfcc(y=data, sr=sampling_rate, n_mfcc=40).T, axis=0)
+            x = np.expand_dims(mfccs, axis=2)
+            x = np.expand_dims(x, axis=0)
+            predictions = self.loaded_model.predict_classes(x)
+            confidence = max(self.loaded_model.predict(x)[0])
+            label = self.convert_class_to_emotion(predictions)
+
+            # writing the data rows 
+            csvwriter.writerow([[file, label, confidence]])
+
+            
 
     @staticmethod
     def convert_class_to_emotion(pred):
@@ -56,8 +73,7 @@ class LivePredictions:
 
 
 if __name__ == '__main__':
-    live_prediction = LivePredictions(file=EXAMPLES_PATH + '03-01-01-01-01-02-05.wav')
+    live_prediction = LivePredictions(folder=DATA_PATH)
     live_prediction.loaded_model.summary()
     live_prediction.make_predictions()
-    live_prediction = LivePredictions(file=EXAMPLES_PATH + '10-16-07-29-82-30-63.wav')
-    live_prediction.make_predictions()
+    
